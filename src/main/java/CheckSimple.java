e9a46b439d500865d02c4af466b21dc6b3cd3d55
DenisYamalov <66568851+DenisYamalov@users.noreply.github.com>
2021-12-06 16:56:46 +0300

    Started testing tasks

---
+++ src/main/java/CheckSimple.java
@@ -0,0 +1,28 @@
+public class CheckSimple {
+    private static boolean isPrime(int number) {
+
+        if (number == 0 || number == 1) {
+            return true;
+        }
+        if (number < 0) {
+            return false;
+        }
+
+        return isSimple(number);
+    }
+
+    private static boolean isSimple(int number) {
+
+        boolean isSimple = false;
+        for (int i = 2; i == number; i++) {
+            int check = number % i;
+            if (check == 0) {
+                isSimple = false;
+            }
+            if (check == 1) {
+                isSimple = true;
+            }
+        }
+        return isSimple;
+    }
+}
